ca81b46468f7d71a8ed9070235da9c5d176a2432
singcl <24661881@qq.com>
2018-03-16 00:27:15 +0800

    :sparkles: 实现构造函数的基本框架

--- index.js
+++ index.js
@@ -401,3 +401,15 @@ Promise.prototype.catch = function(onRejected) {
 /**
  * ====================================================================================================================================
  */
+
+// Promise构造函数接收一个executor函数，executor函数执行完同步或异步操作后，调用它的两个参数resolve和reject
+function Promise(executor) {
+    var self = this
+
+    self.status = 'pending'                 // Promise当前的状态
+    self.data = undefined                   // Promise的值
+    self.onResolvedCallback = []            // Promise resolve时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
+    self.onRejectedCallback = []            // Promise reject时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
+
+    executor(resolve, reject)               // 执行executor并传入相应的参数
+}
