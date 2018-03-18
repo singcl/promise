d07968e009b7a8b912c99f3e88b1563a12f9b986
singcl <24661881@qq.com>
2018-03-18 15:25:32 +0800

    :sparkles: Promise 构造函数和原型方法then方法基本实现

--- index.js
+++ index.js
@@ -410,6 +410,8 @@ function Promise(executor) {
     self.data = undefined                   // Promise的值
     self.onResolvedCallback = []            // Promise resolve时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
     self.onRejectedCallback = []            // Promise reject时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
+
+    /*=================================== Promise 构造函数 ============================================*/
     // resolve函数实现
     // Promise构造函数内部定义的私有函数
     // 作为executor回调函数的第一个参数使用
@@ -418,7 +420,7 @@ function Promise(executor) {
             return value.then(resolve, reject)
         }
         // 异步执行所有回调函数
-        setTimeout(function() {
+        // setTimeout(function() {
             if(self.status === 'pending') {
                 self.status = 'resolved'
                 self.data = value
@@ -426,14 +428,14 @@ function Promise(executor) {
                     self.onResolvedCallback[i](value)
                 }
             }
-        }, 0)
+        // }, 0)
     }
     // reject函数实现
     // Promise构造函数内部定义的私有函数
     // 作为executor回调函数的第二个参数使用
     function reject(reason) {
         // 异步执行所有回调函数
-        setTimeout(function() {
+        // setTimeout(function() {
             if (self.status === 'pending') {
                 self.status = 'rejected'
                 self.data = reason
@@ -441,13 +443,97 @@ function Promise(executor) {
                     self.onRejectedCallback[i](value)
                 }
             }
-        }, 0)
+        // }, 0)
     }
     // executor函数在执行过程中有可能出错
     // 我们来捕获这个错误
-    try {
+    // try {
         executor(resolve, reject)               // 执行executor并传入相应的参数
-    } catch (error) {
-        reject(error)
+    // } catch (error) {
+    //     reject(error)
+    // }
+    /*=============================== Promise 构造函数 END ========================================*/
+
+    /*=============================== Promise 原型方法: then ======================================*/
+    // Promise对象有一个then方法，用来注册在这个Promise状态确定后的回调.
+    // 显然，then方法定义在Promise的原型对象上。then方法会返回一个Promise.
+    Promise.prototype.then = function(onResolved, onRejected) {
+        var self = this
+        var promise2
+
+        // 根据标准，then方法的参数如果不是function,则我们需要忽略它
+        onResolved = typeof onResolved === 'function' ? onResolved : function(v) {}
+        onRejected = typeof onRejected === 'function' ? onRejected : function(r) {}
+
+        if (self.status === 'resolved') {
+            // 如果promise1(此处即为this/self)的状态已经确定并且是resolved，我们调用onResolved
+            // 因为考虑到有可能throw，所以我们将其包在try/catch块里
+            promise2 = new Promise(function(resolve, reject) {
+                try {
+                    var x = onResolved(self.data)
+                    // 如果onResolved的返回值是一个Promise对象，直接取它的结果做为promise2的结果
+                    if (x instanceof Promise) {
+                        x.then(resolve, reject)
+                    }
+                    // 否则，以它的返回值做为promise2的结果
+                    resolve(x)
+                } catch (error) {
+                    // 如果出错，以捕获到的错误做为promise2的结果
+                    reject(error)
+                }
+            })
+            // then方法执行结果返回一个新的promise: promise2
+            return promise2
+        }
+
+        // 此处与前一个if块的逻辑几乎相同，区别在于所调用的是onRejected函数
+        if (self.status === 'rejected') {
+            // 如果promise1(此处即为this/self)的状态已经确定并且是rejected，我们调用onRejected
+            // 因为考虑到有可能throw，所以我们将其包在try/catch块里
+            promise2 = new Promise(function(resolve, reject) {
+                try {
+                    var x = onRejected(self.data)
+                    // 如果onRejected的返回值是一个Promise对象，直接取它的结果做为promise2的结果
+                    if (x instanceof Promise) {
+                        x.then(resolve, reject)
+                    }
+                    // 否则，以它的返回值做为promise2的结果
+                    reject(x)
+                } catch (error) {
+                    // 如果出错，以捕获到的错误做为promise2的结果
+                    reject(error)
+                }
+            })
+            // then方法执行结果返回一个新的promise: promise2
+            return promise2
+        }
+
+        if (self.status === 'pending') {
+            // 如果当前的Promise还处于pending状态，我们并不能确定调用onResolved还是onRejected，
+            // 只能等到Promise的状态确定后，才能确实如何处理。
+            // 所以我们需要把我们的**两种情况**的处理逻辑做为callback放入promise1(此处即this/self)的回调数组里
+            // 逻辑本身跟第一个if块内的几乎一致
+            var promise2 = new Promise(function(resolve, reject) {
+                self.onResolvedCallback.push(function(value) {
+                    var x = onResolved(self.data)
+                    if (x instanceof Promise) {
+                        x.then(resolve, reject)
+                    }
+                    resolve(value)
+                })
+
+                self.onRejectedCallback.push(function(reason) {
+                    var x = onRejected(self.data)
+                    if (x instanceof Promise) {
+                        x.then(resolve, reject)
+                    }
+                    reject(x)
+                })
+            })
+        }
     }
+    /*============================ Promise 原型方法then END =====================================*/
 }
+
+// exports
+module.exports = Promise
