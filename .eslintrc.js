5070a6cf3d5368100e6a070a0134b8712fbcda75
singcl <24661881@qq.com>
2018-03-18 15:42:03 +0800

    :rotating_light: 增加一条lint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-      "max-len": ["error", { "comments": 200 }]
+      "max-len": ["error", { "comments": 200 }],
+      "linebreak-style": 0
     }
 };
