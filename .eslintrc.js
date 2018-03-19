d3cd31e7aa7d8d12e29dc4012a68c168bace97a3
singcl <24661881@qq.com>
2018-03-19 14:42:05 +0800

    :wrench: 更新eslint配置 改用eslint:recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
     "rules": {
       "max-len": ["error", { "comments": 200 }],
       "linebreak-style": 0,
